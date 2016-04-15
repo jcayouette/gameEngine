b33557be8e5d3f678bd15d276dfc92a6184b85e9
jcayouette <jcayouette@suse.com>
2016-04-15 18:05:01 +0200

    added OpenGL GLSL shader support

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -15,30 +16,36 @@ public class MainGameLoop {
         
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
+        
         
         float[] vertices = {
         		//Left bottom triangle
-        		-0.5f,  0.5f, 0f,
-        		-0.5f, -0.5f, 0f,
-        		 0.5f, -0.5f, 0f,
-        		//Right top triangle
-        		 0.5f, -0.5f, 0f,
-        		 0.5f,  0.5f, 0f,
-        		-0.5f,  0.5f, 0f
+        		-0.5f,  0.5f, 0,
+        		-0.5f, -0.5f, 0,
+        		 0.5f, -0.5f, 0,
+        		0.5f,   0.5f, 0
         };
         
-        RawModel model = loader.loadToVAO(vertices);
+        int[] indices = {
+        		
+        		0,1,3, //top left triangle (v0,v1,v3)
+        		3,1,2  //bottom right triangle (v3,v1,v2)
+        };
+        
+        RawModel model = loader.loadToVAO(vertices, indices);
         
         //While display is updating check if a close is requested then execute close display.
         while(!Display.isCloseRequested()) {
-        	renderer.prepare();
         	//game logic
-        	//render
+        	renderer.prepare();
+        	shader.start();
         	renderer.render(model);
+        	shader.stop();
         	DisplayManager.updateDisplay();
         }
         
-        
+        shader.cleanUp();
         loader.cleanUp();
         // closes display when iscloserequested is returned
         DisplayManager.closeDisplay();
--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -1,6 +1,7 @@
 package renderEngine;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,15 +19,16 @@ public class Loader {
 	private List<Integer> vbos = new ArrayList<Integer>();
 	
 	
-	public RawModel loadToVAO(float[] positions) {
+	public RawModel loadToVAO(float[] positions, int[] indices) {
 		/* This method will take in positions of vertices load the data into a VAO then return 
 		 * information about the VAO as a RawModel object.*/
 		int vaoID = createVAO();
+		bindIndicesBuffer(indices);
 		storeDataInAttributeList(0, positions);
 		unbindVAO();
 		
 		//returns raw model data positions length/3 for the 3 vertex positions.
-		return new RawModel(vaoID, positions.length/3);
+		return new RawModel(vaoID, indices.length);
 	
 	}
 	
@@ -63,6 +65,23 @@ public class Loader {
 	private void unbindVAO() {
 		//Unbinds the currently bound ID from createVAO().
 		GL30.glBindVertexArray(0);
+	}
+	
+	private void bindIndicesBuffer(int[] indices){
+		int vboID = GL15.glGenBuffers();
+		vbos.add(vboID);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboID);
+		IntBuffer buffer = storeDataInIntBuffer(indices);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+		
+	}
+	
+	private IntBuffer storeDataInIntBuffer(int[] data) {
+		
+		IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
+		buffer.put(data);
+		buffer.flip();
+		return buffer;
 	}
 	
 	private FloatBuffer storeDataInFloatBuffer(float[] data) {
