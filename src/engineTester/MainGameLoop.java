4aa96147275ed06a7a9dbe514b8a2bb37956f298
jcayouette <jcayouette@suse.com>
2016-04-15 16:50:46 +0200

    first commit

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,47 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        DisplayManager.createDisplay();
+        
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        
+        float[] vertices = {
+        		//Left bottom triangle
+        		-0.5f,  0.5f, 0f,
+        		-0.5f, -0.5f, 0f,
+        		 0.5f, -0.5f, 0f,
+        		//Right top triangle
+        		 0.5f, -0.5f, 0f,
+        		 0.5f,  0.5f, 0f,
+        		-0.5f,  0.5f, 0f
+        };
+        
+        RawModel model = loader.loadToVAO(vertices);
+        
+        //While display is updating check if a close is requested then execute close display.
+        while(!Display.isCloseRequested()) {
+        	renderer.prepare();
+        	//game logic
+        	//render
+        	renderer.render(model);
+        	DisplayManager.updateDisplay();
+        }
+        
+        
+        loader.cleanUp();
+        // closes display when iscloserequested is returned
+        DisplayManager.closeDisplay();
+	}
+
+}
---
+++ src/renderEngine/DisplayManager.java
@@ -0,0 +1,53 @@
+package renderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+
+public class DisplayManager {
+    //Store height and width of game window in these variables
+	private static final int WIDTH = 1920;
+    private static final int HEIGHT = 1080;
+    private static final int FPS_CAP = 120;
+    
+	//creates the game window
+	public static void createDisplay() {
+		//set the version of OpenGL we will be using in this case 3.2
+		ContextAttribs attribs = new ContextAttribs(3,2);
+		
+		//allows use of newer OpenGL versions
+		attribs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+		
+		try {
+			//We need to determine the size of the display
+			Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
+			//now we will actually create the display
+			Display.create(new PixelFormat(), attribs);
+			//sets display title
+			Display.setTitle("Treasure Island");
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//where do we want to render the display? (bottom left of display, top right of display) use the entire screen to render to
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+	}
+	
+	
+	public static void updateDisplay() {
+		//This method updates game window and sets display frame-rate cap
+		Display.sync(FPS_CAP);
+		Display.update();
+	}
+	
+	
+	public static void closeDisplay() {
+		//This method closes game window and cleans up properly
+		Display.destroy();
+	}
+}
